4167d220630a921caa65c88345c36635bd75d3e8
Sarah Vessels <sarah.vessels@cirrusmio.com>
2013-05-16 20:39:13 -0400

    Initial commit of readme, blank plugin file

---
+++ wp-lastfm-charts.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WordPress Last.fm Charts
+Plugin URI:
+Description: This plugin displays charts of Last.fm data for a particular user.
+Version: 1.0
+Author: Sarah Vessels
+Author URI: http://www.3till7.net/
+License: GPLv3
+*/
+?>
