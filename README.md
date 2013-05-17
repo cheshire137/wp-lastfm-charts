4fd2b8da7a7716afc217e83adaddc222cf49a9d2
Sarah Vessels <sarah.vessels@cirrusmio.com>
2013-05-16 22:39:40 -0400

    Horizontal bar charts possible

---
+++ README.md
@@ -0,0 +1,8 @@
+# WordPress Last.fm Charts
+
+* Contributors: cheshire137
+* Tags: last.fm, charts, chart, graphs, pie, visualization, music, artists, songs, tracks
+* License: GPLv3 or later
+* License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+This plugin displays charts of Last.fm data for a particular user.
--- wp-lastfm-charts.php
+++ wp-lastfm-charts.php
@@ -41,42 +41,43 @@ function wp_lastfm_charts_top_artists_shortcode($atts) {
     'chart_fade_color' => 'FFFFFF',
     'width' => '200',
     'height' => '200',
-    'colors' => wp_lastfm_charts_get_colors(),
+    'colors' => wp_lastfm_charts_colors(),
     'limit' => 12,
     'title' => null,
     'data_table_css' => '',
-    'image_alt' => 'Top Artists chart'
+    'image_alt' => 'Top Artists chart',
+    'chart_type' => 'pie'
   ), $atts));
-  $valid_periods = array('overall', '7day', '1month', '3month', '6month',
-                         '12month');
-  if (!in_array($period, $valid_periods)) {
-    $period = 'overall';
-  }
+  $period = wp_lastfm_charts_period($period);
+  $chart_type = wp_lastfm_charts_chart_type($chart_type);
   $title = wp_lastfm_charts_top_artists_title($title, $period, $user_name);
-  // Easy Chart Builder allows up to 12 groups in a chart
-  $limit = ('' == $limit || $limit > 12) ? 12 : $limit;
+  $limit = wp_lastfm_charts_limit($limit);
   $url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user='.
          $user_name.'&api_key='.WP_LASTFM_CHARTS_API_KEY.'&format=json'.
          '&period='.$period.'&limit='.$limit;
   $result = wp_lastfm_charts_get_result($url);
-  $group_names = array();
-  $group_values = array();
-  $artists = $result->{'topartists'}->{'artist'};
-  foreach ($artists as $index => $artist) {
+  $names = array();
+  $values = array();
+  foreach ($result->{'topartists'}->{'artist'} as $artist) {
     $group_name = str_replace(',', ';', $artist->{'name'});
     $group_name = str_replace('"', "'", $group_name);
-    $group_names[] = $group_name;
-    $group_values[] = 'group'.($index + 1).'values="'.$artist->{'playcount'}.
-                      '"';
+    $names[] = $group_name;
+    $values[] = $artist->{'playcount'};
   }
-  $group_names = implode(',', $group_names);
-  $group_values = implode(' ', $group_values);
-  return do_shortcode('[easychart type="pie" groupnames="'.$group_names.'" '.
-                      $group_values.' chartfadecolor="'.$chart_fade_color.
-                      '" chartcolor="'.$chart_color.'" width="'.$width.
-                      '" height="'.$height.'" groupcolors="'.$colors.
-                      '" title="'.$title.'" datatablecss="'.$data_table_css.
-                      '" imagealtattr="'.$image_alt.'"]');
+  return wp_lastfm_charts_easychart(array(
+    'chart_type' => $chart_type,
+    'names' => $names,
+    'values' => $values,
+    'chart_fade_color' => $chart_fade_color,
+    'chart_color' => $chart_color,
+    'width' => $width,
+    'height' => $height,
+    'colors' => $colors,
+    'title' => $title,
+    'data_table_css' => $data_table_css,
+    'image_alt' => $image_alt,
+    'group_name' => 'Artist'
+  ));
 }
 
 function wp_lastfm_charts_top_tracks_shortcode($atts) {
@@ -87,42 +88,72 @@ function wp_lastfm_charts_top_tracks_shortcode($atts) {
     'chart_fade_color' => 'FFFFFF',
     'width' => '200',
     'height' => '200',
-    'colors' => wp_lastfm_charts_get_colors(),
+    'colors' => wp_lastfm_charts_colors(),
     'limit' => 12,
     'title' => null,
     'data_table_css' => '',
-    'image_alt' => 'Top Tracks chart'
+    'image_alt' => 'Top Tracks chart',
+    'chart_type' => 'pie'
   ), $atts));
-  $valid_periods = array('overall', '7day', '1month', '3month', '6month',
-                         '12month');
-  if (!in_array($period, $valid_periods)) {
-    $period = 'overall';
-  }
+  $period = wp_lastfm_charts_period($period);
+  $chart_type = wp_lastfm_charts_chart_type($chart_type);
   $title = wp_lastfm_charts_top_tracks_title($title, $period, $user_name);
-  // Easy Chart Builder allows up to 12 groups in a chart
-  $limit = ('' == $limit || $limit > 12) ? 12 : $limit;
+  $limit = wp_lastfm_charts_limit($limit);
   $url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user='.
          $user_name.'&api_key='.WP_LASTFM_CHARTS_API_KEY.'&format=json'.
          '&period='.$period.'&limit='.$limit;
   $result = wp_lastfm_charts_get_result($url);
-  $group_names = array();
-  $group_values = array();
-  $tracks = $result->{'toptracks'}->{'track'};
-  foreach ($tracks as $index => $track) {
-    $group_name = str_replace(',', ';', $track->{'name'});
-    $group_name = str_replace('"', "'", $group_name);
-    $group_names[] = $group_name;
-    $group_values[] = 'group'.($index + 1).'values="'.$track->{'playcount'}.
-                      '"';
+  $names = array();
+  $values = array();
+  foreach ($result->{'toptracks'}->{'track'} as $track) {
+    $track_name = $track->{'name'};
+    $artist_name = $track->{'artist'}->{'name'};
+    $name = str_replace(',', ';', $track_name.' - '.$artist_name);
+    $name = str_replace('"', "'", $name);
+    $names[] = $name;
+    $values[] = $track->{'playcount'};
   }
-  $group_names = implode(',', $group_names);
-  $group_values = implode(' ', $group_values);
-  return do_shortcode('[easychart type="pie" groupnames="'.$group_names.'" '.
-                      $group_values.' chartfadecolor="'.$chart_fade_color.
-                      '" chartcolor="'.$chart_color.'" width="'.$width.
-                      '" height="'.$height.'" groupcolors="'.$colors.
-                      '" title="'.$title.'" datatablecss="'.$data_table_css.
-                      '" imagealtattr="'.$image_alt.'"]');
+  return wp_lastfm_charts_easychart(array(
+    'chart_type' => $chart_type,
+    'names' => $names,
+    'values' => $values,
+    'chart_fade_color' => $chart_fade_color,
+    'chart_color' => $chart_color,
+    'width' => $width,
+    'height' => $height,
+    'colors' => $colors,
+    'title' => $title,
+    'data_table_css' => $data_table_css,
+    'image_alt' => $image_alt,
+    'group_name' => 'Track'
+  ));
+}
+
+function wp_lastfm_charts_easychart($attribs) {
+  extract($attribs);
+  $shortcode = '[easychart type="'.$chart_type.'" chartfadecolor="'.
+               $chart_fade_color.'" chartcolor="'.$chart_color.
+               '" width="'.$width.'" height="'.$height.'" groupcolors="'.
+               $colors.'" title="'.$title.'" datatablecss="'.
+               $data_table_css.'" imagealtattr="'.$image_alt.'"';
+  switch ($chart_type) {
+    case 'horizbar':
+      $names = implode(',', $names);
+      $values = implode(',', $values);
+      $shortcode .= ' valuenames="'.$names.'" group1values="'.
+                    $values.'" groupnames="'.$group_name.'"';
+      break;
+    case 'pie':
+      foreach ($values as $index => $value) {
+        $values[$index] = 'group'.($index + 1).'values="'.$value.'"';
+      }
+      $names = implode(',', $names);
+      $values = implode(' ', $values);
+      $shortcode .= ' groupnames="'.$names.'" '.$values;
+      break;
+  }
+  $shortcode .= ']';
+  return do_shortcode($shortcode);
 }
 
 function wp_lastfm_charts_top_artists_title($title, $period, $user_name) {
@@ -131,6 +162,23 @@ function wp_lastfm_charts_top_artists_title($title, $period, $user_name) {
 
 function wp_lastfm_charts_top_tracks_title($title, $period, $user_name) {
   return wp_lastfm_charts_title('Top Tracks', $title, $period, $user_name);
+}
+
+function wp_lastfm_charts_chart_type($chart_type) {
+  $valid_chart_types = array('horizbar', 'pie');
+  if (!in_array($chart_type, $valid_chart_types)) {
+    $chart_type = 'pie';
+  }
+  return $chart_type;
+}
+
+function wp_lastfm_charts_period($period) {
+  $valid_periods = array('overall', '7day', '1month', '3month', '6month',
+                         '12month');
+  if (!in_array($period, $valid_periods)) {
+    $period = 'overall';
+  }
+  return $period;
 }
 
 function wp_lastfm_charts_title($prefix, $title, $period, $user_name) {
@@ -148,7 +196,12 @@ function wp_lastfm_charts_title($prefix, $title, $period, $user_name) {
   return $title;
 }
 
-function wp_lastfm_charts_get_colors() {
+function wp_lastfm_charts_limit($limit) {
+  // Easy Chart Builder allows up to 12 groups in a chart
+  return ('' == $limit || $limit > 12) ? 12 : $limit;
+}
+
+function wp_lastfm_charts_colors() {
   return 'b84832,ff9429,ebb736,64ad50,23b07f,1c8f9c,2457b5,4c2cc9,6d23cf,'.
          'b92bcf,ed61cf,c41247';
 }
